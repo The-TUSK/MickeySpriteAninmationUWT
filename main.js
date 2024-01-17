5e8211386ac6606e733901922e4e109f7ba0dee8
Mark Andrey Rubio <marrubio07@gmail.com>
2024-01-17 15:29:52 -0800

    adding new files

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./mickey.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+
+	gameEngine.addEntity(new Mickey(gameEngine));
 
 	gameEngine.init(ctx);
-
+	
 	gameEngine.start();
 });
